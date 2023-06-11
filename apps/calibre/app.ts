4b62ebd97dfcd726fdcdbc7455b44eaae8ded2fb
Braxton Schafer <braxton@cmdcentral.xyz>
2023-06-10 22:02:53 -0500

    feat: add calibre argo app

---
+++ apps/calibre/app.ts
@@ -0,0 +1,20 @@
+import { basename } from "../../lib/util";
+import { App } from "cdk8s";
+import { DEFAULT_APP_PROPS } from "../../lib/consts";
+import { ArgoApp } from "../../lib/argo";
+
+const namespace = basename(__dirname);
+const app = new App(DEFAULT_APP_PROPS(namespace));
+
+new ArgoApp(app, namespace, {
+  sync_policy: {
+    automated: {
+      prune: true,
+      selfHeal: true,
+    },
+  },
+  namespace: namespace,
+  recurse: true,
+});
+
+app.synth();
