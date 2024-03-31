5e27d22b62495d6cfc906fc0e42e7f3a1706a7c9
Braxton Schafer <braxton@cmdcentral.xyz>
2024-03-31 13:11:44 -0500

    feat(bookmarks): back to linkwarden, it seems more better

--- apps/bookmarks/app.ts
+++ apps/bookmarks/app.ts
@@ -1,12 +1,9 @@
-import { App, Chart, Cron, Duration, Size } from "cdk8s";
+import { App, Duration, Size } from "cdk8s";
 import {
-  ConcurrencyPolicy,
   Cpu,
-  CronJob,
   EnvValue,
-  ImagePullPolicy,
+  PersistentVolumeAccessMode,
   Probe,
-  RestartPolicy,
   Secret,
 } from "cdk8s-plus-28";
 import { AppPlus } from "../../lib/app-plus";
@@ -14,12 +11,12 @@ import { ArgoAppSource, NewArgoApp } from "../../lib/argo";
 import { DEFAULT_APP_PROPS } from "../../lib/consts";
 import { NewKustomize } from "../../lib/kustomize";
 import { basename } from "../../lib/util";
+import { StorageClass } from "../../lib/volume";
 
 const namespace = basename(__dirname);
 const name = namespace;
-const image = "ghcr.io/kovah/linkace";
-const semverConstraint = "v1.x.x-simple";
-const port = 80;
+const image = "ghcr.io/linkwarden/linkwarden";
+const port = 3000;
 const app = new App(DEFAULT_APP_PROPS(namespace));
 
 NewArgoApp(name, {
@@ -36,21 +33,18 @@ NewArgoApp(name, {
     images: [
       {
         image: image,
-        versionConstraint: semverConstraint,
-        strategy: "semver",
-        allowTags: String.raw`^v[0-9]+\.[0-9]+\.[0-9]+-simple$`,
+        strategy: "digest",
       },
     ],
   },
 });
 
-const dbCreds = Secret.fromSecretName(app, `${name}-creds`, "db-creds");
-const appKey = Secret.fromSecretName(app, `${name}-appkey`, "appkey");
+const secrets = Secret.fromSecretName(app, `${name}-creds`, "secrets");
 
 new AppPlus(app, `${name}-app`, {
   name: name,
   namespace: namespace,
-  image: `${image}:v1.14.1-simple`,
+  image: image,
   resources: {
     memory: {
       request: Size.mebibytes(256),
@@ -62,19 +56,33 @@ new AppPlus(app, `${name}-app`, {
   },
   ports: [port],
   extraEnv: {
-    APP_KEY: EnvValue.fromSecretValue({ secret: appKey, key: "APP_KEY" }),
-    DB_CONNECTION: EnvValue.fromValue("pgsql"),
-    DB_HOST: EnvValue.fromValue("postgres.cmdcentral.xyz"),
-    DB_DATABASE: EnvValue.fromValue("linkace"),
-    DB_USERNAME: EnvValue.fromValue("linkace"),
-    DB_PORT: EnvValue.fromValue("5432"),
-    DB_PASSWORD: EnvValue.fromSecretValue({
-      secret: dbCreds,
-      key: "DB_PASSWORD",
+    NEXTAUTH_URL: EnvValue.fromValue(
+      "https://bookmarks.cmdcentral.xyz/api/v1/auth",
+    ),
+    NEXTAUTH_SECRET: EnvValue.fromSecretValue({
+      secret: secrets,
+      key: "NEXTAUTH_SECRET",
     }),
-    SETUP_COMPLETED: EnvValue.fromValue("true"), // required for postgres
+    DATABASE_URL: EnvValue.fromSecretValue({
+      secret: secrets,
+      key: "DATABASE_URL",
+    }),
+    NEXT_PUBLIC_DISABLE_REGISTRATION: EnvValue.fromValue("true"),
 
-    SESSION_LIFETIME: EnvValue.fromValue(`${60 * 24 * 365}`), // in minutes, 1 year
+    // Authentik SSO
+    NEXT_PUBLIC_AUTHENTIK_ENABLED: EnvValue.fromValue("true"),
+    AUTHENTIK_CUSTOM_NAME: EnvValue.fromValue("Cmdcentral Login"),
+    AUTHENTIK_ISSUER: EnvValue.fromValue(
+      "https://login.cmdcentral.xyz/application/o/bookmarks",
+    ),
+    AUTHENTIK_CLIENT_ID: EnvValue.fromSecretValue({
+      secret: secrets,
+      key: "AUTHENTIK_CLIENT_ID",
+    }),
+    AUTHENTIK_CLIENT_SECRET: EnvValue.fromSecretValue({
+      secret: secrets,
+      key: "AUTHENTIK_CLIENT_SECRET",
+    }),
   },
   livenessProbe: Probe.fromHttpGet("/", {
     port: port,
@@ -84,49 +92,19 @@ new AppPlus(app, `${name}-app`, {
     port: port,
     initialDelaySeconds: Duration.seconds(5),
   }),
-});
 
-const cronChart = new Chart(app, `${name}-cron`);
-const cronSecret = Secret.fromSecretName(app, `${name}-cron-secret`, "cron");
-
-new CronJob(cronChart, `${name}-cronjob`, {
-  metadata: {
-    name: "cronjob",
-  },
-  schedule: Cron.schedule({ minute: "*/15" }),
-  securityContext: {
-    user: 568,
-    group: 568,
-  },
-  containers: [
+  volumes: [
     {
-      image: "quay.io/curl/curl",
-      imagePullPolicy: ImagePullPolicy.IF_NOT_PRESENT,
-      args: ["curl", "http://bookmarks:80/cron/$(CRON_TOKEN)"],
-      envVariables: {
-        CRON_TOKEN: EnvValue.fromSecretValue({
-          secret: cronSecret,
-          key: "CRON_TOKEN",
-        }),
-      },
-      securityContext: {
-        privileged: false,
-        ensureNonRoot: true,
-        allowPrivilegeEscalation: false,
-        readOnlyRootFilesystem: true,
-      },
-      resources: {
-        memory: {
-          request: Size.mebibytes(32),
-          limit: Size.mebibytes(32),
-        },
+      name: "data",
+      mountPath: "/data/data",
+      enableBackups: true,
+      props: {
+        storage: Size.gibibytes(5),
+        storageClassName: StorageClass.CEPH_RBD,
+        accessModes: [PersistentVolumeAccessMode.READ_WRITE_ONCE],
       },
     },
   ],
-  restartPolicy: RestartPolicy.NEVER,
-  concurrencyPolicy: ConcurrencyPolicy.FORBID,
-  failedJobsRetained: 1,
-  successfulJobsRetained: 1,
 });
 
 app.synth();
