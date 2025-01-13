6f29735308e54a39f9d431c712ef56f18fffa077
Braxton Schafer <braxton@cmdcentral.xyz>
2025-01-13 17:08:17 -0600

    Revert "fix(longhorn): shared resource warnings"
    This reverts commit d946fff7106e204551c09c433c6898253856c5ba.

--- apps/longhorn/app.ts
+++ apps/longhorn/app.ts
@@ -20,7 +20,7 @@ class Longhorn extends Chart {
     new Helm(this, "longhorn", {
       chart: "longhorn",
       repo: "https://charts.longhorn.io",
-      releaseName: namespace, // to avoid SharedResourceWarning
+      releaseName: name,
       namespace: namespace,
       values: {}, // surprisingly
     });
