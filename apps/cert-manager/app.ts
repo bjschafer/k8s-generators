a6857b9c675b2f6a27fa726b6017a195cdd6ad39
Braxton Schafer <braxton@cmdcentral.xyz>
2025-03-04 11:35:51 -0600

    feat: migrate cert-manager to gen

---
+++ apps/cert-manager/app.ts
@@ -0,0 +1,100 @@
+import { App, Chart } from "cdk8s";
+import { basename } from "path";
+import { NewArgoApp } from "../../lib/argo";
+import { DEFAULT_APP_PROPS } from "../../lib/consts";
+import { HelmApp } from "../../lib/helm";
+import { Construct } from "constructs";
+import { ClusterIssuer } from "../../imports/cert-manager.io";
+
+export const namespace = basename(__dirname);
+const name = namespace;
+const app = new App(DEFAULT_APP_PROPS(namespace));
+const version = "1.17.1";
+
+NewArgoApp(name, {
+  namespace: namespace,
+});
+
+new HelmApp(app, "helm", {
+  chart: "cert-manager",
+  repo: "https://charts.jetstack.io",
+  targetRevision: version,
+  releaseName: name,
+  namespace: namespace,
+  values: {
+    crds: {
+      enabled: true,
+    },
+    dns01RecursiveNameservers: "1.1.1.1:53,1.0.0.1:53",
+    dns01RecursiveNameserversOnly: true,
+  },
+});
+
+class CertManagerConfig extends Chart {
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+    const certManagerEmail = "certs@cmdcentral.xyz";
+    const cfEmail = "braxton@cmdcentral.xyz";
+
+    new ClusterIssuer(this, "letsencrypt-staging", {
+      metadata: {
+        name: "letsencrypt-staging",
+      },
+      spec: {
+        acme: {
+          email: certManagerEmail,
+          server: "https://acme-staging-v02.api.letsencrypt.org/directory",
+          privateKeySecretRef: {
+            name: "staging-issuer-account-key",
+          },
+          solvers: [
+            {
+              dns01: {
+                cloudflare: {
+                  email: cfEmail,
+                  apiTokenSecretRef: {
+                    name: "cloudflare-api-token",
+                    key: "api-token",
+                  },
+                },
+              },
+            },
+          ],
+        },
+      },
+    });
+
+    new ClusterIssuer(this, "letsencrypt-prod", {
+      metadata: {
+        name: "letsencrypt",
+      },
+      spec: {
+        acme: {
+          email: certManagerEmail,
+          server: "https://acme-v02.api.letsencrypt.org/directory",
+          privateKeySecretRef: {
+            name: "prod-issuer-account-key",
+          },
+          solvers: [
+            {
+              dns01: {
+                cloudflare: {
+                  email: cfEmail,
+                  apiTokenSecretRef: {
+                    name: "cloudflare-api-token",
+                    key: "api-token",
+                  },
+                },
+              },
+            },
+          ],
+        },
+      },
+    });
+  }
+}
+
+new CertManagerConfig(app, "config");
+
+app.synth();
