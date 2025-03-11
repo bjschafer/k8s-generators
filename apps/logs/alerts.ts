d293f40de3af829fe5fabb4b640fe58ac0c0985c
Braxton Schafer <braxton@cmdcentral.xyz>
2025-03-10 20:25:55 -0500

    fix(alerts): get readonly alert to fire right, hopefully

--- apps/logs/alerts.ts
+++ apps/logs/alerts.ts
@@ -13,6 +13,7 @@ export function addAlerts(scope: Construct, id: string): void {
         expr: `hostname:"k8s" AND job:"systemd-journal" AND "Remounting filesystem read-only" | stats by (hostname) count(*)>0`,
         for: "0m",
         labels: {
+          priority: "0",
           severity: "critical",
           ...SEND_TO_TELEGRAM,
         },
