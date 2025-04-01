1ff1463c9e79d0bf09b752713e1aa807b8299a80
Braxton Schafer <braxton@cmdcentral.xyz>
2025-04-01 11:24:37 -0500

    chore(logs): bump

--- apps/logs/app.ts
+++ apps/logs/app.ts
@@ -20,7 +20,7 @@ import { addAlerts } from "./alerts";
 export const namespace = basename(__dirname);
 const name = namespace;
 const app = new App(DEFAULT_APP_PROPS(namespace));
-const version = "0.9.2";
+const version = "0.9.3";
 
 NewArgoApp(name, {
   sync_policy: {
