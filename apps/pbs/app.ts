b3d4f154c92abdd0ddfe4b0c28812cc019bd590a
Braxton Schafer <braxton@cmdcentral.xyz>
2025-05-13 20:49:27 -0500

    feat: deploy pbs-backup-proxy

---
+++ apps/pbs/app.ts
@@ -0,0 +1,51 @@
+import { basename } from "path";
+import { DEFAULT_APP_PROPS } from "../../lib/consts";
+import { App, Size } from "cdk8s";
+import { NewArgoApp } from "../../lib/argo";
+import { AppPlus } from "../../lib/app-plus";
+import { Cpu } from "cdk8s-plus-32";
+import { NewKustomize } from "../../lib/kustomize";
+
+const namespace = basename(__dirname);
+const name = "pbs-s3-proxy";
+const app = new App(DEFAULT_APP_PROPS(namespace));
+const image = "ghcr.io/tizbac/pmoxs3backuproxy";
+const port = 8007;
+
+NewArgoApp(namespace, {
+  namespace: namespace,
+  autoUpdate: {
+    images: [
+      {
+        image: image,
+        strategy: "digest",
+      },
+    ],
+  },
+});
+
+new AppPlus(app, name, {
+  name: name,
+  namespace: namespace,
+  image: image,
+  ports: [port],
+  resources: {
+    cpu: {
+      request: Cpu.millis(200),
+    },
+    memory: {
+      request: Size.mebibytes(128),
+    },
+  },
+  args: [
+    "-bind",
+    ":8007",
+    "-endpoint",
+    "s3.us-central-1.wasabisys.com:443",
+    "-usessl",
+  ],
+});
+
+app.synth();
+
+NewKustomize(app.outdir);
