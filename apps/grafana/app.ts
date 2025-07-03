c6858c21b6206173fed9ea235c5b3ba624e7d5c9
Braxton Schafer <braxton@cmdcentral.xyz>
2025-07-02 21:27:42 -0500

    Revert "fix(grafana): try a downgrade"
    This reverts commit d8e6dbc81950c63f504cb8485cce0620f1cd8077.

--- apps/grafana/app.ts
+++ apps/grafana/app.ts
@@ -11,7 +11,7 @@ import {
 const namespace = basename(__dirname);
 const name = namespace;
 const app = new App(DEFAULT_APP_PROPS(namespace));
-const version = "8.15.0";
+const version = "9.2.9";
 
 NewArgoApp(name, {
   namespace: namespace,
