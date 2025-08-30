a3c3b1682286c2b1cb2dae000c44d6e76e19872e
Braxton Schafer <braxton@cmdcentral.xyz>
2025-08-30 12:19:09 -0500

    chore(immich): bump

--- apps/immich/app.ts
+++ apps/immich/app.ts
@@ -23,7 +23,7 @@ import { WellKnownLabels } from "../../lib/labels";
 const namespace = basename(__dirname);
 const app = new App(DEFAULT_APP_PROPS(namespace));
 
-const version = "v1.139.4";
+const version = "v1.140.0";
 
 NewArgoApp(namespace, {
   namespace: namespace,
