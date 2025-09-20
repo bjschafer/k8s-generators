fdccb7e4ff7c936acfb8d712759a66b8b4cdf007
Braxton Schafer <braxton@cmdcentral.xyz>
2025-09-20 11:36:27 -0500

    chore(grafana): bump

--- apps/grafana/app.ts
+++ apps/grafana/app.ts
@@ -11,7 +11,7 @@ import {
 const namespace = basename(__dirname);
 const name = namespace;
 const app = new App(DEFAULT_APP_PROPS(namespace));
-const version = "9.4.4";
+const version = "10.0.0";
 
 NewArgoApp(name, {
   namespace: namespace,
