29e957c8fd37975da3f0a18e8666982d25e9fe2f
Braxton Schafer <braxton@cmdcentral.xyz>
2025-10-15 12:18:38 -0500

    chore(eso): bump

--- apps/external-secrets/app.ts
+++ apps/external-secrets/app.ts
@@ -15,7 +15,7 @@ import {
 const namespace = basename(__dirname);
 const name = namespace;
 const app = new App(DEFAULT_APP_PROPS(namespace));
-const version = "0.18.2";
+const version = "0.20.3";
 
 NewArgoApp(name, {
   namespace: namespace,
