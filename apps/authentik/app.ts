ed41cbbeea4ca176444da271c8a0bd18406edbfa
Braxton Schafer <braxton@cmdcentral.xyz>
2025-11-03 14:33:28 -0600

    chore(authentik): bump

--- apps/authentik/app.ts
+++ apps/authentik/app.ts
@@ -24,7 +24,7 @@ import { BitwardenSecret } from "../../lib/secrets";
 const namespace = basename(__dirname);
 const app = new App(DEFAULT_APP_PROPS(namespace));
 
-const version = "2025.10.0";
+const version = "2025.10.1";
 
 NewArgoApp(namespace, {
   namespace: namespace,
