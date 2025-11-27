33c3fb708fc3fe9f0ccaa0c6901a63a72a3846eb
Braxton Schafer <braxton@cmdcentral.xyz>
2025-11-27 13:03:42 -0600

    fix(netboot): tftp is udp

--- apps/netboot/app.ts
+++ apps/netboot/app.ts
@@ -1,6 +1,5 @@
-
 import { App, Duration, Size } from "cdk8s";
-import { Cpu, Probe, ServiceType } from "cdk8s-plus-33";
+import { Cpu, Probe, Protocol, ServiceType } from "cdk8s-plus-33";
 import { basename } from "path";
 import { AppPlus } from "../../lib/app-plus";
 import { NewArgoApp } from "../../lib/argo";
@@ -14,14 +13,14 @@ const image = "ghcr.io/netbootxyz/netbootxyz";
 
 NewArgoApp(namespace, {
   namespace: namespace,
-    autoUpdate: {
-      images: [
-            {
-            image: image,
-            strategy: "digest"
-        }
-      ]
-  }
+  autoUpdate: {
+    images: [
+      {
+        image: image,
+        strategy: "digest",
+      },
+    ],
+  },
 });
 
 const deploy = new AppPlus(app, name, {
@@ -49,33 +48,35 @@ const deploy = new AppPlus(app, name, {
   volumes: [
     {
       name: "config",
-          mountPath: "/config",
-          props: {
-            storage: Size.gibibytes(1),
+      mountPath: "/config",
+      props: {
+        storage: Size.gibibytes(1),
       },
       enableBackups: true,
     },
-      {
-        name: "assets",
-        mountPath: "/assets",
-        props: {
-          storage: Size.gibibytes(1),
-        },
-        enableBackups: true,
-    }
+    {
+      name: "assets",
+      mountPath: "/assets",
+      props: {
+        storage: Size.gibibytes(1),
+      },
+      enableBackups: true,
+    },
   ],
-})
+});
 
 deploy.Deployment.exposeViaService({
-    name: `${name}-tftp`,
-    ports: [{
-        port: 69,
-        targetPort: 69,
-    name: "tftp",
-    }],
-    serviceType: ServiceType.LOAD_BALANCER,
-    
-})
+  name: `${name}-tftp`,
+  ports: [
+    {
+      port: 69,
+      targetPort: 69,
+      name: "tftp",
+      protocol: Protocol.UDP,
+    },
+  ],
+  serviceType: ServiceType.LOAD_BALANCER,
+});
 
 app.synth();
 NewKustomize(app.outdir);
