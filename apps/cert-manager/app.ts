4aa7b5c06291013b58f1a6bc5c7688f7a4a1d6ef
Braxton Schafer <braxton@cmdcentral.xyz>
2025-12-13 14:06:12 -0600

    chore(cert-manager): bump

--- apps/cert-manager/app.ts
+++ apps/cert-manager/app.ts
@@ -10,7 +10,7 @@ import { AddCRDs } from "../../lib/util";
 export const namespace = basename(__dirname);
 const name = namespace;
 const app = new App(DEFAULT_APP_PROPS(namespace));
-const version = "1.19.1";
+const version = "1.19.2";
 
 NewArgoApp(name, {
   namespace: namespace,
