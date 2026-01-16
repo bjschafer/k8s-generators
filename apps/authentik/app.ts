21d719c97587dffd24df53823a22c2257ff9aa7b
Braxton Schafer <braxton@cmdcentral.xyz>
2026-01-15 20:12:19 -0600

    chore(authentik): upgrade to 2025.12.0

--- apps/authentik/app.ts
+++ apps/authentik/app.ts
@@ -24,7 +24,7 @@ import { BitwardenSecret } from "../../lib/secrets";
 const namespace = basename(__dirname);
 const app = new App(DEFAULT_APP_PROPS(namespace));
 
-const version = "2025.10.3";
+const version = "2025.12.0";
 
 NewArgoApp(namespace, {
   namespace: namespace,
