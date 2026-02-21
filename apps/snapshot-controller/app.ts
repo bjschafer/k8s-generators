4a17ffbec4936886acc3309e3362c8915fa21865
Braxton Schafer <braxton@cmdcentral.xyz>
2026-02-21 14:38:16 -0600

    chore(ceph)!: migrate snapshot controller to generators

---
+++ apps/snapshot-controller/app.ts
@@ -0,0 +1,35 @@
+import { App } from "cdk8s";
+import { NewArgoApp, ArgoAppSource, ENABLE_SERVERSIDE_APPLY } from "../../lib/argo";
+import { DEFAULT_APP_PROPS } from "../../lib/consts";
+import { HelmApp } from "../../lib/helm";
+import { NewKustomize } from "../../lib/kustomize";
+
+const name = "snapshot-controller";
+const namespace = "ceph";
+const app = new App(DEFAULT_APP_PROPS(name));
+
+NewArgoApp(name, {
+  namespace,
+  directoryName: name,
+  source: ArgoAppSource.GENERATORS,
+  ...ENABLE_SERVERSIDE_APPLY,
+});
+
+new HelmApp(app, "helm", {
+  chart: "snapshot-controller",
+  repo: "https://piraeus.io/helm-charts/",
+  version: "5.0.3",
+  releaseName: name,
+  namespace,
+  values: {
+    controller: {
+      replicaCount: 2,
+    },
+    webhook: {
+      enabled: false,
+    },
+  },
+});
+
+app.synth();
+NewKustomize(app.outdir);
